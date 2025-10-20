7d840d9bcadbcf61dd2705f65b20f4ba85fa1401
kostyaKobel1 <kostp72323@gmail.com>
2025-10-20 19:11:57 +0300

    All content from files has been extracted into one file and Refactored code

--- js-tasks/makeNegative.js
+++
@@ -1,9 +0,0 @@
-// 2 - In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
-function makeNegative(num) {
-  if (num > 0) {
-    return -num;
-  }
-  else {
-    return num;
-  }
-}
--- js-tasks/personalizedMessage.js
+++
@@ -1,9 +0,0 @@
-// 4- Create a function that gives a personalized greeting. This function takes two parameters: name and owner. 
-function greet(name, owner) {
-  if (name === owner) {
-    return 'Hello boss';
-  }
-  else {
-    return 'Hello guest';
-  }
-}
