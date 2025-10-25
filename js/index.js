0ca21d2633c0f493bfcb58ae9dd96775e3d42a7d
Batmar78 <igorovyc@gmail.com>
2025-10-26 00:17:28 +0300

    changed file index.customer-rewiews

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Marjan Batkivskiy
+function init() {
+  import('./index.customer-revievs-carusel.js');
+}
+
+const totalPartials = document.querySelectorAll(
+  '[hx-trigger="load"], [data-hx-trigger="load"]',
+).length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) init();
+});
