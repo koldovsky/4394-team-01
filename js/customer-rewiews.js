2c547d0f8913c8c4d5a712e6e92eb545e3e79f27
Batmar78 <igorovyc@gmail.com>
2025-10-26 16:40:31 +0200

    code correction

--- js/customer-rewiews.js
+++ js/customer-rewiews.js
@@ -1,23 +1,23 @@
-import { starsTemplate } from './starsteamplate.js';
+import { starsTeamplate } from './starsteamplate.js';
 
 export const customerReviews = [
   {
-    stars: starsTemplate,
+    stars: starsTeamplate,
     text: 'We opted for a tent overlooking the tranquil lake, and it was the perfect setting for a peaceful retreat.',
     person: 'Jessica Wilson, Tampa'
   },
   {
-    stars: starsTemplate,
+    stars: starsTeamplate,
     text: 'The geodesic dome we stayed in was a unique experience, offering unparalleled views of the star-studded sky at night.',
     person: 'Amanda Lowson, New York'
   },
   {
-    stars: starsTemplate,
+    stars: starsTeamplate,
     text: 'We enjoyed exploring the nearby trails for mountain biking, and the kids had a blast roasting marshmallows over the campfire.',
     person: 'Dean Peterson, Nebraska'
   },
   {
-    stars: starsTemplate,
+    stars: starsTeamplate,
     text: 'Hidden Heaven Glamping exceeded all our expectations. The tree house accommodation was like a fairytale!',
     person: 'Eric Wastler, California'
   }
