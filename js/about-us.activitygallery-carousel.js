e022b3f87183e0e0d4062773fcb983fb12fb98c0
Natalka Tkhoriv <natalka.tkhoriv@gmail.com>
2025-10-27 22:07:46 +0100

    changes

--- js/about-us.activitygallery-carousel.js
+++ js/about-us.activitygallery-carousel.js
@@ -3,3 +3,14 @@ const modalImage = document.getElementById('modalImage');
 const [zoomInBtn, zoomOutBtn] = document.querySelectorAll('.gallery__zoom-buttons button');
 const closeBtn = document.getElementById('closeGalleryModal');
 const [prevBtn, nextBtn] = document.querySelectorAll('.gallery__modal-nav-buttons button');
+
+const galleryImages = document.querySelectorAll('.activity__gallery-images img');
+let currentIndex = 0;
+galleryImages.forEach((img, index) => {
+  img.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    currentIndex = index;
+    modalImage.src = img.src;
+    modalImage.style.transform = 'scale(1)';
+  });
+});
