b3767216f44d8326daa7797a8e6c47f26bbf4c25
Natalka Tkhoriv <natalka.tkhoriv@gmail.com>
2025-10-27 21:53:40 +0100

    changes

---
+++ js/about-us.activitygallery-carousel.js
@@ -0,0 +1,5 @@
+const modal = document.getElementById('galleryModal');
+const modalImage = document.getElementById('modalImage');
+const [zoomInBtn, zoomOutBtn] = document.querySelectorAll('.gallery__zoom-buttons button');
+const closeBtn = document.getElementById('closeGalleryModal');
+const [prevBtn, nextBtn] = document.querySelectorAll('.gallery__modal-nav-buttons button');
